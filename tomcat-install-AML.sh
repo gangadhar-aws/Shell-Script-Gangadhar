853cc6521cf7c3d0437eb556ae1e40ea155b0721
kumari@gmail.com <kumari>
2024-04-28 20:08:13 +0530

    tomcat install

---
+++ tomcat-install-AML.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /opt/
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.88/bin/apache-tomcat-9.0.88.tar.gz
+tar -xvzf apache-tomcat-9.0.88.tar.gz
+cd apache-tomcat-9.0.88/
+cd bin/
+
